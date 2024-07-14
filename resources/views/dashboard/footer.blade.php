2f2219ff3d5fd83451f23dc88f8a425a4abb0e69
dhianr <nurfadhiad@gmail.com>
2024-07-15 02:06:58 +0700

    Praktikum 12-13

---
+++ resources/views/dashboard/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+        <b>Praktikum Laravel 11</b>
+    </div>
+    <strong>Copyright &copy; 2024 <a href="https://www.linkedin.com/in/dinda-nurfadhia-73abb0249/">Dinda
+            Nurfadhia</a>.</strong> SI-04
+</footer>
